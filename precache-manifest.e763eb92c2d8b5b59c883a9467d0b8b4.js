15e08b47d1f76d462515a1da77cb4de0ae7468ad
LyG <ylorozcoy@gmail.com>
2020-08-21 17:12:27 -0500

    Updates

---
+++ precache-manifest.e763eb92c2d8b5b59c883a9467d0b8b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99392b8dc83494ec38c8138910349353",
+    "url": "/LyG/index.html"
+  },
+  {
+    "revision": "0546f3b6b817cb5ed641",
+    "url": "/LyG/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f97528078f08d8689be1",
+    "url": "/LyG/static/js/2.2ebf5461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LyG/static/js/2.2ebf5461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0546f3b6b817cb5ed641",
+    "url": "/LyG/static/js/main.52d07a76.chunk.js"
+  },
+  {
+    "revision": "67a8763f9d3fdafef20a",
+    "url": "/LyG/static/js/runtime-main.de75db50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LyG/static/media/logo.5d5d9eef.svg"
+  }
+]);
